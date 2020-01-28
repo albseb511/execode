7591691172e198aa7c8fba215dbcb488a49f4818
albseb511 <albertsebastian511@gmail.com>
2020-01-28 14:37:41 +0530

    Update: layout fix, code refactor, terminologies

--- src/client/src/routes/Dashboard/Admin/AdminDashboard.jsx
+++ src/client/src/routes/Dashboard/Admin/AdminDashboard.jsx
@@ -6,60 +6,41 @@ const AdminDashboard = () => {
     <div className="container">
       <h1>Admin Dashboard</h1>
       <div>
-        <div className="row border mb-3 mt-5">
-          <div className="col-md-8">
-            <div className="py-2">
-              <div className="card-body">
-                This is some text within a card body.
+        {[
+          {
+            path: "all-contest",
+            body: "View all your contests",
+            linkTitle: "Contests"
+          },
+          {
+            path: "create-contest",
+            body:
+              "Create a new contest. A contest should have atleast one challenge/problem",
+            linkTitle: "Create Contest"
+          },
+          {
+            path: "create-challenge",
+            body: "Create new problem statements or challenges for users",
+            linkTitle: "Create Problems"
+          }
+        ].map(card => (
+          <div className="row border mb-3 mt-5">
+            <div className="col-md-7">
+              <div className="py-2">
+                <div className="card-body">{card.body}</div>
               </div>
             </div>
-          </div>
-          <div className="col-md-3 py-3">
-            <Link
-              className="btn btn-outline-dark btn-block btn-lg"
-              to="/dashboard/admin/all-contest"
-              role="button"
-            >
-              Contests
-            </Link>
-          </div>
-        </div>
-        <div className="row border mb-3 mt-4">
-          <div className="col-md-8">
-            <div className="py-2">
-              <div className="card-body">
-                This is some text within a card body.
-              </div>
+            <div className="col-md-4 py-3">
+              <Link
+                className="btn btn-outline-dark btn-block btn-lg"
+                to={`/dashboard/admin/${card.path}`}
+                role="button"
+              >
+                {card.linkTitle}
+              </Link>
             </div>
           </div>
-          <div className="col-md-3 py-3">
-            <Link
-              className="btn btn-outline-dark btn-block btn-lg"
-              to="/dashboard/admin/create-contest"
-              role="button"
-            >
-              Create Contest
-            </Link>
-          </div>
-        </div>
-        <div className="row border mb-3 mt-4">
-          <div className="col-md-8">
-            <div className="py-2">
-              <div className="card-body">
-                This is some text within a card body.
-              </div>
-            </div>
-          </div>
-          <div className="col-md-3 py-3">
-            <Link
-              className="btn btn-outline-dark btn-block btn-lg"
-              to="/dashboard/admin/create-challenge"
-              role="button"
-            >
-              Create Challenge
-            </Link>
-          </div>
-        </div>
+        ))}
       </div>
     </div>
   );
