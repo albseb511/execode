ac54718f311b6f743a9b8cb7cfaeba2b2722a065
vetrivel chinnasamy <26738977+vetrivelcsamy@users.noreply.github.com>
2020-01-29 18:08:33 +0530

    Challenge Leaderboard Modifed

--- src/client/src/routes/Dashboard/User/Challenge/ChallengeLeaderboard.jsx
+++ src/client/src/routes/Dashboard/User/Challenge/ChallengeLeaderboard.jsx
@@ -3,7 +3,28 @@ import React from "react";
 function ChallengeLeaderboard() {
   return (
     <div className="container">
-      <h1>ChallengeLeaderboard</h1>
+      <div className="mt-5 mb-4">
+        <h3 className="font-weight-bold">Challenge Leaderboard</h3>
+      </div>
+      <table className="table">
+        <thead className="thead-dark">
+          <tr>
+            <th scope="col">
+              <i className="fas fa-crown" />
+              <span> Rank</span>
+            </th>
+            <th scope="col">Username</th>
+            <th scope="col">Total Mark</th>
+          </tr>
+        </thead>
+        <tbody>
+          <tr>
+            <th scope="row">1</th>
+            <td>Mark</td>
+            <td>Otto</td>
+          </tr>
+        </tbody>
+      </table>
     </div>
   );
 }
