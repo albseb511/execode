c2b7bdbb909265bb91aab8d9c0679d7eaebf24a2
albseb511 <albertsebastian511@gmail.com>
2020-01-30 16:20:04 +0530

    Update: pathname as prop for redirect

--- src/client/src/routes/Routes.jsx
+++ src/client/src/routes/Routes.jsx
@@ -15,7 +15,7 @@ const Routes = () => {
       <Route path="/" component={NavBarPublic} />
       <Switch>
         <Route path="/" exact render={() => <Home />} />
-        <Route path="/dashboard" render={() => <DashboardRoutes />} />
+        <Route path="/dashboard" render={({location}) => <DashboardRoutes path={location.pathname} />} />
         <Route path="/login" render={() => <Login />} />
         <Route path="/register" render={() => <Register />} />
         <Route path="/about" render={() => <About />} />
