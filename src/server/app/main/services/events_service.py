5eff594a2d6dac5ae0961533e8100094e5b8542e
Parth <parth.verma96@gmail.com>
2020-01-30 15:22:31 +0530

    Bug Fix in query

--- src/server/app/main/services/events_service.py
+++ src/server/app/main/services/events_service.py
@@ -22,7 +22,7 @@ def add_event(data,user_id):
     return save_changes(new_assest)
 
 def get_events(user_id):
-    data_event = db.engine.execute("select a.event as event, a.created_at as created_at, a.event_text as text, b.name as name, d.challenge_name from events as a join contests_challenges as c on c.id = a.contests_challenges_id join challenges as d on d.id = c.challenge_id join users as b on b.id = a.%s;"%(user_id))
+    data_event = db.engine.execute("select a.event as event, a.created_at as created_at, a.event_text as text, b.name as name, d.challenge_name from events as a join contests_challenges as c on c.id = a.contests_challenges_id join challenges as d on d.id = c.challenge_id join users as b on b.id = a.id where a.user_id = '%s';"%(user_id))
     events = []
     for row in data_event:
         temp_dict = {}
