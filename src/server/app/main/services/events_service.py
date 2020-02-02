7864953cb39056b82c4e86b7716fe4353720c302
Parth <parth.verma96@gmail.com>
2020-02-02 12:44:21 +0530

    added order by in events fetching query

--- src/server/app/main/services/events_service.py
+++ src/server/app/main/services/events_service.py
@@ -22,7 +22,7 @@ def add_event(data,user_id):
     return save_changes(new_assest)
 
 def get_events(user_id):
-    data_event = db.engine.execute("select a.event as event, a.created_at as created_at, a.event_text as text, b.name as name, d.challenge_name from events as a join contests_challenges as c on c.id = a.contests_challenges_id join challenges as d on d.id = c.challenge_id join users as b on b.id = a.user_id where a.user_id = %s;"%(user_id))
+    data_event = db.engine.execute("select a.event as event, a.created_at as created_at, a.event_text as text, b.name as name, d.challenge_name from events as a join contests_challenges as c on c.id = a.contests_challenges_id join challenges as d on d.id = c.challenge_id join users as b on b.id = a.user_id where a.user_id = %s ORDER BY YEAR(a.created_at) DESC, MONTH(a.created_at) DESC, DAY(a.created_at) DESC, HOUR(a.created_at) DESC, MINUTE(a.created_at) DESC, SECOND(a.created_at) DESC;;"%(user_id))
     events = []
     for row in data_event:
         temp_dict = {}
