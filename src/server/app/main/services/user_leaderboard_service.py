1185fba1ec4c2a91df8817b8ba25738a03475973
Parth <parth.verma96@gmail.com>
2020-02-02 12:40:48 +0530

    added contest name in leaderboard response

--- src/server/app/main/services/user_leaderboard_service.py
+++ src/server/app/main/services/user_leaderboard_service.py
@@ -10,7 +10,7 @@ import json
 
 def get_raw_data(contest_id):
 
-    data_raw = db.engine.execute("select total.user_id, total.total, total.contest_id, u.name, u.email from (select sum(max_score) as total, user_id, contest_id from attempts where contest_id = %s group by user_id) as total join users as u on u.id = total.user_id;"%(contest_id))
+    data_raw = db.engine.execute("select total.user_id, total.total, total.contest_id, u.name, u.email, c.contest_name from (select sum(max_score) as total, user_id, contest_id from attempts where contest_id = %s group by user_id) as total join users as u on u.id = total.user_id join contests as c on c.id = total.contest_id;"%(contest_id))
     print(data_raw)
     names = []
     #names = [dict(row) for  row in data_raw]
