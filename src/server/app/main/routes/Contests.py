6b085fe212aa219b3d6819199745c2d264008312
Parth <parth.verma96@gmail.com>
2020-03-16 16:01:01 +0530

    Updated: contests for user signed up contests only

--- src/server/app/main/routes/Contests.py
+++ src/server/app/main/routes/Contests.py
@@ -3,9 +3,15 @@ from flask_restful import Resource
 from ..models.ContestsModel import ContestsModel
 from ..services.contest_detail import get_contests
 from app.main import db
+from app.main.services.decode_auth_token import decode_auth_token
 
 
 class Contests(Resource):
     @classmethod
     def get(self):
-        return get_contests()
+        auth_token = request.headers.get("Authorization")
+        user_id = decode_auth_token(auth_token)
+        if user_id:
+            return get_contests(user_id)
+        else:
+            return {"comment": "JWT Expired or Invalid"}, 200
