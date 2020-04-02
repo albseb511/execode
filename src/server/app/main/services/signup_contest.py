02041ec8a5e3dce86a993b58cc707c8b0d2c7eb1
Parth <parth.verma96@gmail.com>
2020-04-03 02:02:13 +0530

    check if the signup is already present with proper error messages

--- src/server/app/main/services/signup_contest.py
+++ src/server/app/main/services/signup_contest.py
@@ -14,19 +14,24 @@ def save_changes(data):
         print(e)
 
 def add_signup(data, user_id):
-    new_asset = SignUpContestModel(contest_id=data['contest_id'],
-                            user_id = user_id)
-
-    save_changes(new_asset)
-    signup_id = new_asset.id
-    if signup_id == None:
-        return False
+    prev_signup = SignUpContestModel.query.filter_by(
+            contest_id=data['contest_id'], user_id = user_id).first()
+    if prev_signup:
+        return {"created":False, "comment": "user already signed up"}, 200
     else:
-        return True
+        new_asset = SignUpContestModel(contest_id=data['contest_id'],
+                                user_id = user_id)
 
-def validate_signup(data, user_id):
+        save_changes(new_asset)
+        signup_id = new_asset.id
+        if signup_id == None:
+            return {"created":True, "comment": "signed up successfully"}, 200
+        else:
+            return {"created":False, "comment": "error in signing up"}, 200
+
+def validate_signup(contest_id, user_id):
     signup_record = SignUpContestModel.query.filter_by(
-        contest_id=data['contest_id'], user_id = user_id).first()
+        contest_id=contest_id, user_id = user_id).first()
     if signup_record:
         return True
     else:
